ba6b462c8ab4a1f7ac633e2ddd81e46fca0f07dc
Crafterbot <jackchupp@icloud.com>
2023-06-17 21:41:46 -0400

    Re-added scoreboard & speedboost

---
+++ SpeedBoostExample/SpeedBoostExample/Main.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using Utilla;
+
+namespace SpeedBoost
+{
+    [BepInPlugin(GUID, Name, Version)]
+    [BepInDependency("Crafterbot.MonkeStatistics")]
+
+    [ModdedGamemode]
+    internal class Main : BaseUnityPlugin
+    {
+        internal const string
+            GUID = "crafterbot.speedboostexample",
+            Name = "SpeedBoost",
+            Version = "1.0.0";
+        internal static bool RoomValid;
+        internal static bool Enabled;
+
+        internal static float SpeedBoost = 1.0f;
+
+        private void Awake()
+        {
+            new Harmony(GUID).PatchAll(Assembly.GetExecutingAssembly());
+            MonkeStatistics.API.Registry.AddAssembly();
+        }
+
+        [ModdedGamemodeJoin]
+        private void Join() =>
+            RoomValid = true;
+        [ModdedGamemodeLeave]
+        private void Leave() =>
+            RoomValid = false;
+    }
+}
